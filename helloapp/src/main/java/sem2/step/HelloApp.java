fdfdc9790312b6c6cd4d0ef6b6c4c514cd67deab
DakshSwami <dakshswami212@gmail.com>
2026-03-18 10:52:23 +0530

    Coded UC1: Display Hello World in console

--- helloapp/src/main/java/sem2/step/HelloApp.java
+++ helloapp/src/main/java/sem2/step/HelloApp.java
@@ -1,13 +1,20 @@
-package sem2.step;
-
 /**
- * Hello world!
- *
+ * HelloApp.java - A simple Java application that Displays "Hello, World!" to 
+ * the console.
+ * 
+ * This is the first step in the HelloApp journey, where we start with a basic
+ * "Hell, World!" program. The application consists of a single class with a
+ * main method that serves as the entry point for the program. The main method
+ * uses the System.out.println() function to print the message "Hello, World!"
+ * to the console. This application demonstrates fundamental Java concepts such
+ * as class declaration, the main method, and standard output.
+ * 
+ * @author Daksh
+ * @version 1.0
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class HelloApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
